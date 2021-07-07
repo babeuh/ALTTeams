8f73d91ad4afa8498bccca57b8af7cab4e2f999b
victivus <victivus@pm.me>
2021-07-07 16:12:54 +0200

    Update dash.tsx

--- ahig/src/pages/dash.tsx
+++ ahig/src/pages/dash.tsx
@@ -1,11 +1,3 @@
-import { GetServerSideProps } from "next";
-
 import { DashPage } from "../modules/pages/dashboard/DashPage";
 
 export default DashPage;
-
-export let getServerSideProps: GetServerSideProps = async (ctx) => {
-  return {
-    props: {},
-  };
-};
